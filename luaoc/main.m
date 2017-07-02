dddc05b1fb8f4a462e6fa41a10ee58a885f3635d
Tajoy <tj328111241@gmail.com>
2017-07-02 11:02:26 +0800

    Initial Commit

---
+++ luaoc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  luaoc
+//
+//  Created by tajoy on 2017/7/2.
+//  Copyright © 2017年 tajoy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
